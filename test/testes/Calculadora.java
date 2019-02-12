f46f8799156fe733d7bc856fa924a0c3f071f654
EmersonPaduan <emerson.paduan@gmail.com>
2019-02-12 16:37:49 -0200

    adicionado teste para metodo soma

---
+++ test/testes/Calculadora.java
@@ -0,0 +1,9 @@
+package testes;
+
+class Calculadora {
+
+    double soma(double n1, double n2) {
+        return n1 + n2;
+    }
+
+}
--- test/testes/CalculadoraJUnitTest.java
+++ test/testes/CalculadoraJUnitTest.java
@@ -1,8 +1,24 @@
 package testes;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class CalculadoraJUnitTest {
     
     public CalculadoraJUnitTest() {
     }
     
+    @Test
+    public void testSomaValoresPositivos(){
+        Calculadora c = new Calculadora();
+        double resp = c.soma(10,10);
+        assertEquals("Soma de valores positivos", 20, resp, 0);
+    }
+
+    @Test
+    public void testSomaValoresNegativos(){
+        Calculadora c = new Calculadora();
+        double resp = c.soma(-10,-10);
+        assertEquals("Soma de valores positivos", -20, resp, 0);
+    }    
 }
